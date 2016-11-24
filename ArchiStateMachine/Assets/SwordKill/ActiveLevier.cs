f6c120acc4c1f8c0f13bffec292f029f5222d7b7
Elencrak <hansotte.joachim@gmail.com>
2016-11-24 12:01:31 +0100

    Player dead open door

---
+++ ArchiStateMachine/Assets/SwordKill/ActiveLevier.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActiveLevier : MonoBehaviour {
+
+    private PropertyHolder propertyHolder;
+    public PropertyHolder refToActive;
+    // Use this for initialization
+    void Start () {
+        propertyHolder = GetComponent<PropertyHolder>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (propertyHolder.GetProperty("activated") == "true")
+        {
+            refToActive.SetProperty("opened", "true");
+        }
+        else if (propertyHolder.GetProperty("activated") == "false")
+        {
+            refToActive.SetProperty("opened", "false");
+        }
+    }
+}
---
+++ ArchiStateMachine/Assets/SwordKill/CollisionDetection.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class CollisionDetection : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider trigger)
+    {
+        if(trigger.tag == "sword")
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
